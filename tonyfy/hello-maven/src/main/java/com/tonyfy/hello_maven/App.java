d58347492bc071a83d04ea83cfe59978ac204ad9
tonyfy <tony.yakovlenko@gmail.com>
2012-10-15 14:30:23 +0300

    Imrove formatting

--- tonyfy/hello-maven/src/main/java/com/tonyfy/hello_maven/App.java
+++ tonyfy/hello-maven/src/main/java/com/tonyfy/hello_maven/App.java
@@ -3,9 +3,11 @@ package com.tonyfy.hello_maven;
 import org.apache.commons.lang3.StringUtils;
 
 public class App {
+
     public static void main(String[] args) {
-        String hello = "Hello World!"; 
+        String hello = "Hello World!";
 
         System.out.println(StringUtils.replace(hello, "World", "Maven"));
     }
+
 }
