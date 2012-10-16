0c7efe48f0666521a350e4592a912b3137a0c08a
vctrmrcnsk <vctrmrcnsk@yahoo.com>
2012-10-16 20:09:48 +0300

    target folder must not be included

--- vctrmrcnsk/first_maven/src/main/java/com/vctrmrcnsk/first_maven/Grep.java
+++
@@ -1,9 +0,0 @@
-package com.vctrmrcnsk.first_maven;
-import org.apache.commons.lang3.StringUtils;
-public class Grep {
-	/* checks if given token contains the search string and prints it, if true */
-	public static void printMatch(String token, String search) {
-		if (StringUtils.contains(token, search))
-			System.out.println(token);
-	}
-}
