3dec8cd70531f4a9df26423548bc5cb28757deab
Bostanica Ion <kemonoske@gmail.com>
2012-10-17 12:29:46 -0900

    add map with  maven project

---
+++ kemonoske/parseurl/src/main/java/parser/Main.java
@@ -0,0 +1,15 @@
+package parser;
+
+public class Main {
+	
+	public static void main(String[] args)	{
+		
+		if(args.length == 0)
+			System.err.println("No arguments passed.");
+		
+		for(String i : args)
+			System.out.println((new URL(i)).resolve());
+		
+	}
+
+}
