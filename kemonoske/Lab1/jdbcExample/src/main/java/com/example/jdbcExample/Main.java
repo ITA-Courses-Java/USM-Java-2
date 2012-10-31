aada1ac7cd68dc5640ca1511183459a4217207d2
Bostanica Ion <kemonoske@gmail.com>
2012-10-31 07:58:36 -0900

    Playing with git cmd

--- kemonoske/Lab1/jdbcExample/src/main/java/com/example/jdbcExample/Main.java
+++ kemonoske/Lab1/jdbcExample/src/main/java/com/example/jdbcExample/Main.java
@@ -1,14 +1,17 @@
 package com.example.jdbcExample;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class Main {
-
+	
 	public static void main(String[] args) throws ClassNotFoundException, SQLException{
 		
 		StudentDAO sd = new StudentDAO();
 		
-		System.out.println(sd.listStudents());
+		List aux = sd.listStudents();
+		
+		System.out.println(sd);
 		
 	}
 	
