3faff0666164d37c0df533f74e8c4ec783a54d6c
kemonoske <kemonoske@gmail.com>
2012-10-31 23:14:22 +0200

    whatever

--- kemonoske/Lab1/jdbcExample/src/main/java/com/example/jdbcExample/Main.java
+++ kemonoske/Lab1/jdbcExample/src/main/java/com/example/jdbcExample/Main.java
@@ -8,13 +8,7 @@ public class Main {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException{
 		
 		StudentDAO sd = new StudentDAO();
-<<<<<<< HEAD
-		sd.listStudents();
-=======
-		
 		List aux = sd.listStudents();
-		
->>>>>>> b95268ab46f1c0464f9dd3d47cd15a34e33a71e4
 		System.out.println(sd);
 		
 	}
